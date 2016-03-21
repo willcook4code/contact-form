2a470cbc99146c289efadcf76a6e897d46d9d755
Will Cook <wellsee83@gmail.com>
2016-03-21 18:49:10 -0500

    Up to color delay on button

---
+++ main.js
@@ -0,0 +1,51 @@
+var form = $('form');
+var email = $('#email');
+var emailErr = $('#emailErr');
+var website = $('#website');
+var message = $('#message');
+var nameErr = $('#nameErr');
+var userName = $('#userName');
+var webErr = $('#webErr');
+var msgErr = $('#msgErr');
+
+var error = function () {
+	$('p').css('color', 'red');
+	if (userName.val() === '') {
+		nameErr.html("Name cannot be left empty");
+	} else {
+		nameErr.html("");
+	}
+	if (email.val() === '') {
+		emailErr.html("Email cannot be left empty");
+	} else if (email.val().includes('@')===false){
+		emailErr.html("Email must contain an '@'.");
+	} else {
+		emailErr.html('');
+	}
+	if (website.val() === '') {
+		webErr.html("Website cannot be left empty");
+	} else if (website.val().startsWith('http://')===false){
+		webErr.html("Website must start with 'http://'.");
+	} else {
+		webErr.html('');
+	}
+	if (message.val() === '') {
+		msgErr.html("Name cannot be left empty");
+	} else {
+		msgErr.html("");
+	}
+
+};
+
+
+form.on('submit', function(e){
+	e.preventDefault();
+	error();
+});
+
+$('button').mouseover(function() {
+	$('button').animate(500, function() {
+		$('button').css({'background': '#88C470'})})});
+
+
+
